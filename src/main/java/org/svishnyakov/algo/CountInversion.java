72305d4c4ddfdbe5f18483a4052ccc5d4a186697
Sergey Vishnyakov <sergey.vishnyakov@opower.com>
2017-11-28 13:35:52 +0200

    Coursera task: Count number of inversions using merge sort

---
+++ src/main/java/org/svishnyakov/algo/CountInversion.java
@@ -0,0 +1,76 @@
+package org.svishnyakov.algo;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Count number of inversion by leveraging merge sort algorithm.
+ *
+ * For example: given array [3, 2, 1]. There are 3 inversions here (3, 1), (3, 2) and (2, 1)
+ * The inversion considered to be pair of numbers where {@literal i < j and array[i] > array[j]}
+ */
+public class CountInversion {
+
+    private static long totalInversions = 0;
+
+    public static void main(String[] args) throws Exception {
+        int[] numbers = loadNumbers("/algo/countInversions.txt");
+
+        int[] sortedNumbers = countAndSort(numbers);
+        System.out.println("Sorted arrays: " + Arrays.toString(sortedNumbers));
+        System.out.println("Total number of inversions: " + totalInversions);
+
+    }
+
+    private static int[] loadNumbers(String filePath) throws Exception {
+        Stream<String> lines = Files.lines(Paths.get(CountInversion.class.getResource(filePath).toURI()));
+        return lines.mapToInt(Integer::valueOf).toArray();
+    }
+
+    private static int[] countAndSort(int[] numbers) {
+        if (numbers.length < 2) {
+            return numbers;
+        }
+
+        int splitNumber = numbers.length / 2;
+        // The fact that we copy arrays here does not make out algorithm super fast, but I preferred this approach to
+        // the solution where I pass the same array but have to play around with start and end indexes.
+        int[] firstHalf = countAndSort(Arrays.copyOfRange(numbers, 0, splitNumber));
+        int[] secondHalf = countAndSort(Arrays.copyOfRange(numbers, splitNumber, numbers.length));
+        return mergeAndCountInversions(firstHalf, secondHalf);
+    }
+
+    private static int[] mergeAndCountInversions(int[] firstHalfSorted, int[] secondHalfSorted) {
+        List<Integer> sortedResult = new ArrayList<>(firstHalfSorted.length + secondHalfSorted.length);
+        int f = 0;
+        int s = 0;
+        while (f < firstHalfSorted.length && s < secondHalfSorted.length) {
+            if (secondHalfSorted[s] <= firstHalfSorted[f]) {
+                totalInversions += firstHalfSorted.length - f;
+                sortedResult.add(secondHalfSorted[s]);
+                s++;
+            }
+            else {
+                sortedResult.add(firstHalfSorted[f]);
+                f++;
+            }
+        }
+
+        if (f < firstHalfSorted.length) {
+            for (int i = f; i < firstHalfSorted.length; i++) {
+                sortedResult.add(firstHalfSorted[i]);
+            }
+        }
+        else if (s < secondHalfSorted.length) {
+            for (int i = s; i < secondHalfSorted.length; i++) {
+                sortedResult.add(secondHalfSorted[i]);
+            }
+        }
+
+        return sortedResult.stream().mapToInt(i -> i).toArray();
+    }
+}
